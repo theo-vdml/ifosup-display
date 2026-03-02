02083d7f1ca529041e3021674b712c9e98a42d1b
Théo Vandormael <theo.vandormael@proton.me>
2026-03-02 11:28:07 +0100

    feat: add course model and migration

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Teacher extends Model
 {
     protected $fillable = ['name'];
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
 }
