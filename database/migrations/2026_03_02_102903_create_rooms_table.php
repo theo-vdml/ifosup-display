eb4363d3dfe0403d6a47030e34623ccb4511d671
Théo Vandormael <theo.vandormael@proton.me>
2026-03-02 11:32:43 +0100

    feat: update room migration

--- database/migrations/2026_03_02_102903_create_rooms_table.php
+++ database/migrations/2026_03_02_102903_create_rooms_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
         });
     }
 
