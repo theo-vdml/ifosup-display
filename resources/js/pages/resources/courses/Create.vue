10fd6869037791eaa46d2bd1cb6f41248a926073
Théo Vandormael <theo.vandormael@proton.me>
2026-03-10 16:45:05 +0100

    ui: adding a headlessui based combobox

--- resources/js/pages/resources/courses/Create.vue
+++ resources/js/pages/resources/courses/Create.vue
@@ -1,24 +1,42 @@
 <script setup lang="ts">
     import actions from '@/actions/App/Http/Controllers/CourseController';
+    import BaseCombobox from '@/components/BaseCombobox.vue';
     import InputError from '@/components/InputError.vue';
     import { Input } from '@/components/ui/input';
     import { Label } from '@/components/ui/label';
     import { useResourceRoutes } from '@/composables/useResourceRoutes';
     import ResourceFormLayout from '@/layouts/resources/ResourceFormLayout.vue';
+    import { ref } from 'vue';
 
     const routes = useResourceRoutes(null, actions);
+
+    const options = [
+        { id: 1, name: 'Option 1' },
+        { id: 2, name: 'Option 2' },
+        { id: 3, name: 'Option 3' },
+    ];
+
+    const selectedOption = ref(null);
 </script>
 
 <template>
 
 
     <ResourceFormLayout title="Nouveau Cours" description="Ajoutez un nouveau cours dans la base de données."
-                        type="Cours" :routes="routes" :form-action="actions.store.form()">
+        type="Cours" :routes="routes" :form-action="actions.store.form()">
         <template #default="{ errors }">
+            <BaseCombobox v-model="selectedOption" placeholder="Code du cours" :options="options" :display-value="(option) => option.name" />
+
+            <div class="grid gap-2">
+                <Label for="code">Code du cours</Label>
+                <Input id="code" class="mt-1 block w-full" name="code" required autocomplete="code"
+                    placeholder="Code du cours" />
+                <InputError class="mt-2" :message="errors.code" />
+            </div>
             <div class="grid gap-2">
                 <Label for="name">Nom du cours</Label>
                 <Input id="name" class="mt-1 block w-full" name="name" required autocomplete="name"
-                       placeholder="Nom du cours" />
+                    placeholder="Nom du cours" />
                 <InputError class="mt-2" :message="errors.name" />
             </div>
         </template>
--- resources/js/pages/resources/courses/Edit.vue
+++ resources/js/pages/resources/courses/Edit.vue
@@ -17,12 +17,18 @@
 
 
     <ResourceFormLayout :title="course.name" description="Modifiez les informations du cours." type="Cours"
-                        :routes="routes" :form-action="actions.update.form(props.course.id)" :is-edit="true">
+        :routes="routes" :form-action="actions.update.form(props.course.id)" :is-edit="true">
         <template #default="{ errors }">
+            <div class="grid gap-2">
+                <Label for="code">Code du cours</Label>
+                <Input id="code" class="mt-1 block w-full" name="code" required autocomplete="code"
+                    placeholder="Code du cours" :default-value="course.code" />
+                <InputError class="mt-2" :message="errors.code" />
+            </div>
             <div class="grid gap-2">
                 <Label for="name">Nom du cours</Label>
                 <Input id="name" class="mt-1 block w-full" name="name" required autocomplete="name"
-                       placeholder="Nom complet" :default-value="course.name" />
+                    placeholder="Nom complet" :default-value="course.name" />
                 <InputError class="mt-2" :message="errors.name" />
             </div>
         </template>
